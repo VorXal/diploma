d7fb9f9ec86246ebfe8c4b27b5f0ffab64d66c1f
Vorxal <alexey32101231@gmail.com>
2021-02-26 00:23:42 +0400

    Release working version of application

--- src/components/theory.component.js
+++ src/components/theory.component.js
@@ -4,7 +4,80 @@ export default class Theory extends Component {
 
     render() {
         return (
-            <p>Страница с теорией</p>
+            <div className="content">
+                <div className="card-columns">
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%94%D0%B8%D1%81%D0%BA%D0%BE%D0%BD%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D0%BE%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D1%8C" className="card-text">Дисконтированная стоимость (Present Value)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://psyera.ru/budushchaya-stoimost-deneg_12014.htm" className="card-text">Будущая стоимость (Future Value)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D1%82%D0%B0%D1%8F_%D0%BF%D1%80%D0%B8%D0%B2%D0%B5%D0%B4%D1%91%D0%BD%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D0%BE%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D1%8C" className="card-text">Чистая приведённая стоимость (Net Present Value)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%A1%D1%80%D0%BE%D0%BA_%D0%BE%D0%BA%D1%83%D0%BF%D0%B0%D0%B5%D0%BC%D0%BE%D1%81%D1%82%D0%B8" className="card-text">Срок окупаемости (Payback Period)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%9E%D0%BA%D1%83%D0%BF%D0%B0%D0%B5%D0%BC%D0%BE%D1%81%D1%82%D1%8C_%D0%B8%D0%BD%D0%B2%D0%B5%D1%81%D1%82%D0%B8%D1%86%D0%B8%D0%B9" className="card-text">Коэффициент возврата инвестиций (Return On Investment)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D0%BE_72" className="card-text">Правило 72 (Rule 72)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%90%D0%BC%D0%BE%D1%80%D1%82%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F_(%D1%84%D0%B8%D0%BD%D0%B0%D0%BD%D1%81%D1%8B)" className="card-text">Амортизация (Amortiztion)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D0%B4%D0%B5%D0%BA%D1%81_%D1%80%D0%B5%D0%BD%D1%82%D0%B0%D0%B1%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8" className="card-text">Индекс доходности (Profitability Index)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%A1%D1%82%D0%B0%D0%B2%D0%BA%D0%B0_%D0%B4%D0%B8%D1%81%D0%BA%D0%BE%D0%BD%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F" className="card-text">Коэффициент дисконтирования (Discount Factor)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%A1%D0%BB%D0%BE%D0%B6%D0%BD%D1%8B%D0%B5_%D0%BF%D1%80%D0%BE%D1%86%D0%B5%D0%BD%D1%82%D1%8B" className="card-text">Сложные проценты (Compound Interest)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/CAGR" className="card-text">Совокупный среднегодовой темп роста(Compound Annual Growth Rate)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BD%D0%B0%D0%BD%D1%81%D0%BE%D0%B2%D1%8B%D0%B9_%D1%80%D1%8B%D1%87%D0%B0%D0%B3" className="card-text">Финансовый рычаг (Leverage Ratio)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%A1%D1%80%D0%B5%D0%B4%D0%BD%D0%B5%D0%B2%D0%B7%D0%B2%D0%B5%D1%88%D0%B5%D0%BD%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D0%BE%D0%B8%D0%BC%D0%BE%D1%81%D1%82%D1%8C_%D0%BA%D0%B0%D0%BF%D0%B8%D1%82%D0%B0%D0%BB%D0%B0" className="card-text">Средневзвешенная стоимость капитала (Weight Average Cost of Capital)</a>
+                        </div>
+                    </div>
+                    <div className="card bg-light">
+                        <div className="card-body text-center">
+                            <a href="https://ru.wikipedia.org/wiki/%D0%90%D0%BD%D0%BD%D1%83%D0%B8%D1%82%D0%B5%D1%82" className="card-text">Платеж по кредиту (Loan Payment)</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
